04168de7ea7384a5e6ba392f93149fcf5d98766d
Gavin <m.gavin.aga10@gmail.com>
2021-10-26 13:04:04 +0700

    feat[MGA]set-realmmodel

--- app/src/main/java/com/danta/sidqi/phonestore/Model.java
+++ app/src/main/java/com/danta/sidqi/phonestore/Model.java
@@ -1,4 +1,38 @@
 package com.danta.sidqi.phonestore;
 
-public class Model {
+import java.io.Serializable;
+
+import io.realm.RealmObject;
+
+public class Model extends RealmObject implements Serializable{
+    private String Brand;
+    private String PhoneName;
+    private String Image;
+
+    public Model() {
+    }
+
+    public String getBrand() {
+        return Brand;
+    }
+
+    public void setBrand(String brand) {
+        Brand = brand;
+    }
+
+    public String getPhoneName() {
+        return PhoneName;
+    }
+
+    public void setPhoneName(String phoneName) {
+        PhoneName = phoneName;
+    }
+
+    public String getImage() {
+        return Image;
+    }
+
+    public void setImage(String image) {
+        Image = image;
+    }
 }
